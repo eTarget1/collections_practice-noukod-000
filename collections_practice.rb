b5c8666fd4a3ff18cbd4154cff60121a242445ee
clifford <clifford@noukod.com>
2018-09-24 14:38:02 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,6 +1,23 @@
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ @eTarget1 Sign out
+1
+0 1 thebrianemory/flatiron-refresh
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Insights
+flatiron-refresh/collections_practice-v-000/collections_practice.rb
+a5fa8b1  on Jan 25, 2017
+@thebrianemory thebrianemory Done
+     
+56 lines (47 sloc)  801 Bytes
 def sort_array_asc(array)
-  array.sort do |a,b|
-    a <=>
+  array.sort do |a, b|
+    a <=> b
   end
 end
 
@@ -24,3 +41,45 @@ end
 def reverse_array(array)
   array.reverse
 end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |name|
+    new_array << name[2] = "$"
+  end
+end
+
+def find_a(array)
+  all_a = []
+  array.each do |name|
+    all_a << name if name[0].downcase == "a"
+  end
+  all_a
+end
+
+def sum_array(array)
+  array.inject(0) { |sum, i| sum + i }
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index == 1
+      element
+    else
+      element << "s"
+    end
+  end
+end
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Press h to open a hovercard with more details.
